e2646e2cd6a4bb32b8bb346c9a387d24633c3233
Paulo Paiva <paulojr.eco@gmail.com>
2023-10-20 14:48:21 -0300

    Imersao Full Cycle class 2 completed

---
+++ application/usecase/transaction.go
@@ -0,0 +1,86 @@
+package usecase
+
+import (
+	"log"
+
+	"github.com/paulojr-eco/codepix-go/domain/model"
+)
+
+type TransactionUseCase struct {
+	TransactionRepository model.TransactionRepositoryInterface
+	PixKeyRepository      model.PixKeyRepositoryInterface
+}
+
+func (t *TransactionUseCase) Register(accountId string, amount float64, pixKeyTo string, pixKeyKindTo string, description string) (*model.Transaction, error) {
+	account, err := t.PixKeyRepository.FindAccount(accountId)
+	if err != nil {
+		return nil, err
+	}
+
+	pixKey, err := t.PixKeyRepository.FindKeyByKind(pixKeyTo, pixKeyKindTo)
+	if err != nil {
+		return nil, err
+	}
+
+	transaction, err := model.NewTransaction(account, amount, pixKey, description)
+	if err != nil {
+		return nil, err
+	}
+
+	err = t.TransactionRepository.Register(transaction)
+	if err != nil {
+		return nil, err
+	}
+
+	return transaction, nil
+}
+
+func (t *TransactionUseCase) Confirm(transactionId string) (*model.Transaction, error) {
+	transaction, err := t.TransactionRepository.Find(transactionId)
+	if err != nil {
+		log.Println("transaction not found", transactionId)
+		return nil, err
+	}
+
+	transaction.Status = model.TransactionConfirmed
+	err = t.TransactionRepository.Save(transaction)
+	if err != nil {
+		return nil, err
+	}
+
+	return transaction, nil
+}
+
+func (t *TransactionUseCase) Complete(transactionId string) (*model.Transaction, error) {
+	transaction, err := t.TransactionRepository.Find(transactionId)
+	if err != nil {
+		log.Println("transaction not found", transactionId)
+		return nil, err
+	}
+
+	transaction.Status = model.TransactionCompleted
+	err = t.TransactionRepository.Save(transaction)
+	if err != nil {
+		return nil, err
+	}
+
+	return transaction, nil
+}
+
+func (t *TransactionUseCase) Error(transactionId string, reason string) (*model.Transaction, error) {
+	transaction, err := t.TransactionRepository.Find(transactionId)
+	if err != nil {
+		log.Println("transaction not found", transactionId)
+		return nil, err
+	}
+
+	transaction.Status = model.TransactionError
+	transaction.CancelDescription = reason
+
+	err = t.TransactionRepository.Save(transaction)
+	if err != nil {
+		return nil, err
+	}
+
+	return transaction, nil
+}
